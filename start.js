d33639bc8495920cd48a75dbf9baa76302e17b66
Rohit Subash Nair <rohitsubash.nair@philips.com>
2026-09-01 02:01:08 +0530

    Configure CTF challenge for Render

--- start.js
+++ start.js
@@ -30,7 +30,7 @@ app.get('/api/status', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
   console.log(`Visit http://localhost:${PORT} and manually save to archive.org`);
